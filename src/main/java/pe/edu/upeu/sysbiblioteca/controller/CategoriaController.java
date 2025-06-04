97965d8238bd0be0530ce83aa93fec9b1bc54594
docente.fia <docente.fia@host.docker.internal>
2025-06-04 17:46:33 -0500

    v1.6

--- src/main/java/pe/edu/upeu/sysbiblioteca/controller/CategoriaController.java
+++ src/main/java/pe/edu/upeu/sysbiblioteca/controller/CategoriaController.java
@@ -1,5 +1,26 @@
 package pe.edu.upeu.sysbiblioteca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import pe.edu.upeu.sysbiblioteca.service.CategoriaService;
+import pe.edu.upeu.sysbiblioteca.service.ICategoriaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class CategoriaController {
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("cats", categoriaService.readAll());
+		return "index";
+	}
 
 }
--- src/main/java/pe/edu/upeu/sysbiblioteca/daoImpl/CategoriaDaoImpl.java
+++ src/main/java/pe/edu/upeu/sysbiblioteca/daoImpl/CategoriaDaoImpl.java
@@ -1,41 +1,50 @@
 package pe.edu.upeu.sysbiblioteca.daoImpl;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import pe.edu.upeu.sysbiblioteca.dao.CategoriaDao;
 import pe.edu.upeu.sysbiblioteca.entity.Categoria;
 
 @Repository
 public class CategoriaDaoImpl implements CategoriaDao{
-
-	
+	@Autowired
+   private JdbcTemplate jdbc;
+   	
 	@Override
 	public int create(Categoria t) {
-		// TODO Auto-generated method stub
-		return 0;
+		String SQL = "INSERT INTO categorias (nombre,estado) VALUES(?,1)";
+		return jdbc.update(SQL,new Object[] {t.getNombre()});
 	}
 
 	@Override
 	public int update(Categoria t) {
-		// TODO Auto-generated method stub
-		return 0;
+		String SQL = "UPDATE INTO categorias SET nombre=? WHERE id=?";
+		return jdbc.update(SQL,new Object[] {t.getNombre(), t.getIdcategoria()});
 	}
 
 	@Override
 	public int delete(int id) {
 		// TODO Auto-generated method stub
-		return 0;
+		String SQL = "DELETE FROM categorias where id=?";
+		return jdbc.update(SQL,new Object[] {id});
 	}
 
 	@Override
 	public Categoria read(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		String SQL = "SELECT *FROM categoria WHERE id=?";		
+		 Categoria cat = jdbc.queryForObject(SQL, BeanPropertyRowMapper.newInstance(Categoria.class), id);
+	        return cat;
 	}
 
 	@Override
 	public List<Categoria> readAll() {
 		// TODO Auto-generated method stub
-		return null;
+		String SQL = "SELECT *FROM categorias";
+		return jdbc.query(SQL,BeanPropertyRowMapper.newInstance(Categoria.class));
 	}
 }
